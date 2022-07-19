421be52a3e78af8e1ca869f54f16b6bb56dc1cb7
Guilherme Cintra <guilhermecintra@Guilhermes-MacBook-Pro.local>
2022-07-19 20:35:37 -0300

    simple tauri command runned from cljs

--- ui-cljs/src/ui_cljs/views.cljs
+++ ui-cljs/src/ui_cljs/views.cljs
@@ -1,14 +1,23 @@
 (ns ui-cljs.views
   (:require
    [re-frame.core :as re-frame]
-   [ui-cljs.subs :as subs]
-   ))
+   [ui-cljs.subs :as subs]))
 
+(defn window->tauri []
+  (println "window->tauri"))
 
+(def invoke (. js/window -__TAURI_INVOKE__))
+
+(def ob #js {"name" "world!"})
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (re-frame/subscribe [::subs/name])
+        greetx (window->tauri)]
     [:div.container.px-4 {:style {:margin 80}}
      [:h1
       "Hello from " @name]
-     [:h2 "yes!"]]))
+     [:h2 "yes!"]
+     
+     [:button 
+      {:on-click (fn [_] (.then (invoke "greet" ob) #(println %)))} 
+      "Click me!"]]))
