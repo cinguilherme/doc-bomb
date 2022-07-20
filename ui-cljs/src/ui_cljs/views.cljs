3a77c725601cc847bcf74f663653f514f468f06d
Cintra, Guilherme <cinguilherme@gmail.com>
2022-07-20 09:57:40 -0300

    update view

--- ui-cljs/src/ui_cljs/views.cljs
+++ ui-cljs/src/ui_cljs/views.cljs
@@ -1,23 +1,25 @@
 (ns ui-cljs.views
   (:require
+   
    [re-frame.core :as re-frame]
    [ui-cljs.subs :as subs]))
 
-(defn window->tauri []
-  (println "window->tauri"))
-
 (def invoke (. js/window -__TAURI_INVOKE__))
+
+(defn ->js-object [m]
+  (js/object m))
 
 (def ob #js {"name" "world!"})
 
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])
-        greetx (window->tauri)]
+  (let [name (re-frame/subscribe [::subs/name])]
     [:div.container.px-4 {:style {:margin 80}}
      [:h1
       "Hello from " @name]
      [:h2 "yes!"]
      
      [:button 
-      {:on-click (fn [_] (.then (invoke "greet" ob) #(println %)))} 
+      {:on-click (fn [_] (-> (invoke "greet" ob) 
+                             (.then  #(println %))))} 
       "Click me!"]]))
