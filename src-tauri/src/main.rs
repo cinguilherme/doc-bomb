d80bba970b507d87ca0b07484a6a5a971f865390
Cintra, Guilherme <cinguilherme@gmail.com>
2022-07-21 10:47:57 -0300

    fib async not locking frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,12 @@
 
 use tauri::command;
 
-fn fib_internal(n: u64) -> u64 {
+async fn fib_internal(n: u64) -> u64 {
   // fibonacci tail recursive 
+
+  //sleep thread for 500ms
+  std::thread::sleep(std::time::Duration::from_millis(500));
+
   fn fib_tail(n: u64, a: u64, b: u64) -> u64 {
     if n == 0 {
       a
@@ -18,8 +22,10 @@ fn fib_internal(n: u64) -> u64 {
 }
 
 #[command]
-fn fib(n: u64) -> u64 {
-  fib_internal(n)
+async fn fib(n: u64) -> u64 {
+  let res = fib_internal(n).await;
+  println!("fib({}) = {}", n, res);
+  return res
 }
 
 #[command]
