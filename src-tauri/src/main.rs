e5df41b444192cede63b2066946f3a7972665517
Cintra, Guilherme <cinguilherme@gmail.com>
2022-07-21 10:28:22 -0300

    tauri commands working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,34 @@
   windows_subsystem = "windows"
 )]
 
-#[tauri::command]
+use tauri::command;
+
+fn fib_internal(n: u64) -> u64 {
+  if n == 0 {
+    0
+  } else if n == 1 {
+    1
+  } else {
+    fib_internal(n - 1) + fib_internal(n - 2)
+  }
+}
+
+#[command]
+fn fib(n: u64) -> String {
+  println!("fib({})", n);
+  let res = fib_internal(n);
+  res.to_string()
+}
+
+#[command]
 fn greet(name: &str) -> String {
+  println!("Hello, {}!", name);
   format!("Hello, {}!", name)
 }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![greet, fib])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- ui-cljs/src/ui_cljs/views.cljs
+++ ui-cljs/src/ui_cljs/views.cljs
@@ -1,25 +1,50 @@
 (ns ui-cljs.views
-  (:require-macros [devcards.core :as dc :refer [defcard deftest]])
   (:require [re-frame.core :as re-frame]
             [ui-cljs.subs :as subs]))
 
+(defn tap [v] (println v) v)
+
 (def invoke (. js/window -__TAURI_INVOKE__))
 
-(defn ->js-object [m]
-  (js/object m))
+(defn evt-value [e]
+  (. (. e -target) -value))
 
-(def ob #js {"name" "world!"})
+(defn fibonacci-backend [n]
+  (int n)
+  (->> #js {"n" (int n)}
+       tap 
+       (invoke "fib")
+       tap))
 
-(defcard card [:h1 "hey"])
+(defn greet-backend []
+  (->> #js {"name" "world"}
+       tap 
+       (invoke "greet")
+       tap))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div.container.px-4 {:style {:margin 80}}
+     
      [:h1
       "Hello from " @name]
+     
      [:h2 "yes!"]
 
      [:button
-      {:on-click (fn [_] (-> (invoke "greet" ob)
-                             (.then  #(println %))))}
-      "Click me!"]]))
+      {:on-click (fn [_] (do (println "invoked click!")
+                             (-> (greet-backend)
+                                 (.then  #(println %)))))}
+      "Click me!"]
+
+     [:br]
+
+     [:div.container.px-4
+      [:label "Fibonacci Calc"]
+      [:input
+
+       {:type "number"
+        :on-change (fn [e] (-> (evt-value e)
+                               fibonacci-backend
+                               (.then  #(println %))
+                               (.catch #(println "unable to resolve promise: " %))))}]]]))
